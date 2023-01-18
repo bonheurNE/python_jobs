8a712d82afd49d6a4821945511cfba713321b3ad
ndeze_bonheur <ndezeBE@gmail.com>
2023-01-18 20:55:45 +0200

    first comit to the main branch

---
+++ main.py
@@ -0,0 +1,21 @@
+# importing necessary buildin modules
+import os
+import json
+
+# importing necessary external modules
+import requests
+import lxml
+from bs4 import Beautifuulsoup
+
+
+def get_jobs(url:str) -> None:
+    pass
+
+
+# function  to run the main function code
+if __name__ == "__main__":
+    # the url link to the oficial python available jobs
+    url_link = ""
+    
+    # the main function 
+    get_jobs(url=url_link)
